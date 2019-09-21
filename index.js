c5f6b171c5acc69140b051286f27389b428f3914
Ralph Mallett <ralph_mallett@hotmail.com>
2019-09-21 15:18:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var price = Math.round(Math.random() * 100);
  }  
 
 function viewCart() {
-  if (cart.length > 0) {return `In your car, you have ${item} at ${price}`}
+  if (cart.length > 0) {return `In your cart, you have ${item} at ${price}`}
  else {return "your cart is currently empty"}
 
   // write your code here
