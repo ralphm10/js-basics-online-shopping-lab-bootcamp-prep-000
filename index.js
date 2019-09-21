0400898bee593ca1a078e20618351e1e03f45849
Ralph Mallett <ralph_mallett@hotmail.com>
2019-09-21 14:59:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,12 +12,13 @@ function setCart(c) {
 function addToCart(item) {
  // write your code here
  
- if (cart.length > 0) 
+ if (cart.length > 0) {
  var price = Math.round(Math.random() * 100);
  var itemdetail = {itemName: item, itemPrice: price};
  cart.push(itemdetail);
  return `${item} has been added to your cart.`; 
  }
+}
 
 function viewCart() {
   // write your code here
